aa7e9ff166201e34c70ddd9399330261ee35032b
Evgenchick4434 <zhekatv@bk.ru>
2026-02-26 20:16:56 +0200

    Update steam.ps1

--- steam.ps1
+++ steam.ps1
@@ -1,3 +1,16 @@
+$BasePath = "$env:USERPROFILE\Downloads"
+$ZipPath = "$BasePath\Scoop-main.zip"
+
+Invoke-WebRequest -Uri "https://github.com/ScoopInstaller/Scoop/archive/refs/heads/main.zip" -OutFile $ZipPath
+
+if (Test-Path "$BasePath\Scoop-main") {
+    Remove-Item "$BasePath\Scoop-main" -Recurse -Force
+}
+
+Expand-Archive $ZipPath -DestinationPath $BasePath -Force
+
+Set-Location "$BasePath\Scoop-main"
+
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 irm get.scoop.sh | iex
